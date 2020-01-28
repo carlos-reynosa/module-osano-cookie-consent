7cf2b599e54224ae576713af45a8ff2854a26628
Carlos Reynosa <carlos.reynosa@pdp.com>
2020-01-28 15:03:48 -0800

    Add Feature - Give the ability to change the cookie consent popup background and button background within the admin.

--- Block/Init/Config.php
+++ Block/Init/Config.php
@@ -6,6 +6,11 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
+/**
+ * Class reading system configuration for the cookie consent plugin
+ * and transforming it into a format understood by the plugin.
+ * @package CarlosReynosa\OsanoCookieConsent\Block\Init
+ */
 class Config implements ArgumentInterface
 {
     protected $scopeConfig;
@@ -19,8 +24,8 @@ class Config implements ArgumentInterface
      */
     public function __construct(ScopeConfigInterface $scopeConfig, Json $jsonSerializer)
     {
-        $this->scopeConfig=$scopeConfig;
-        $this->jsonSerializer =$jsonSerializer;
+        $this->scopeConfig = $scopeConfig;
+        $this->jsonSerializer = $jsonSerializer;
     }
 
     /**
@@ -40,6 +45,28 @@ class Config implements ArgumentInterface
      */
     public function getConfigJson()
     {
-        return $this->jsonSerializer->serialize($this->getConfig());
+        $initConfig = $this->systemConfigToInitConfig($this->getConfig());
+        return $this->jsonSerializer->serialize($initConfig);
+    }
+
+    /**
+     * Convert system config values into array structure that can be easily turned into the js plugin init configuration
+     *
+     * @param array $config Valid Osano Cooke Consent js plugin configuration for use with the init function.
+     * @return array
+     * @see https://www.osano.com/cookieconsent/documentation/javascript-api/
+     */
+    protected function systemConfigToInitConfig(array $config)
+    {
+        return [
+            'palette' => [
+                'popup' => [
+                    'background' => $config['palette_popup_background']
+                ],
+                'button' => [
+                    'background' => $config['palette_button_background']
+                ]
+            ]
+        ];
     }
 }
